08c0d6d09d48f6cff9204bef0307e7755389118a
Aung Myat Hein <championboylay@gmail.com>
2020-11-16 21:57:54 +0630

    - Changed images path for production

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -410,8 +410,8 @@ export class MainComponent implements OnInit {
     this.showGridEachAnswer()
     this.round4QuestionList.forEach(question => {
       console.log('question => ', question)
-      let color = 'url(../../../assets/images/grid-normal-bg.png) no-repeat' //'#013ad6';
-      if (question.roundFourStatus == 'skip') color = 'url(../../../assets/images/grid-skip-bg.png) no-repeat' //'#eeeeee75'
+      let color = 'url(./assets/images/grid-normal-bg.png) no-repeat' //'#013ad6';
+      if (question.roundFourStatus == 'skip') color = 'url(./assets/images/grid-skip-bg.png) no-repeat' //'#eeeeee75'
       question.hints.forEach(hint => {
         hint.position.forEach((id, index) => {
           // console.log('id: ', id, ', ans: ', question.ans)
@@ -437,7 +437,7 @@ export class MainComponent implements OnInit {
 
     this.showedAnswerList.forEach((question, index) => {
       let color;
-      if (question.roundFourStatus === 'correct') color = 'url(../../../assets/images/grid-correct-bg.png) no-repeat' //'#48B822'
+      if (question.roundFourStatus === 'correct') color = 'url(./assets/images/grid-correct-bg.png) no-repeat' //'#48B822'
       // else if (question.roundFourStatus === 'wrong') color = '#F00';
       // else color = 'rebeccapurple'
       question.hints.forEach(hint => {
