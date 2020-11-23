cf9f17301a8c870de0c65d3f893783a7f9fa16cd
UNayThura <nay.thura.94@gmail.com>
2020-11-23 12:47:34 +0630

    remove tube for ruound three

--- src/app/views/oneThirdScreen/oneThirdScreen.component.ts
+++ src/app/views/oneThirdScreen/oneThirdScreen.component.ts
@@ -253,6 +253,11 @@ export class OneThirdScreenComponent implements OnInit {
       this.image.style.backgroundSize = "cover";
       // remove round two background image
       this.roundTwoBgImageEle.style.background = ""
+    } else if (this.currentRound.questionType == 3) {
+      this.cube_image.src = ''              // remove cube image
+      this.image.style.background = "url(" + this.oneThirdBgImage.src + ")";
+      this.image.style.backgroundSize = "cover";
+      this.roundTwoBgImageEle.style.background = ""
     }
     else {
       this.cube_image.src = this.cubeImage.src;
