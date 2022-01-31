792311183690780b6fe5c0332c0391e87dd2acb7
Zin Bhone Htut <ZinBhoneHtut@startinpoint.com>
2022-01-31 15:06:04 +0630

    enhance (animation) : add animation for buttons and answer words

--- backend/WordWhizExternalDevTunnel/src/main/resources/static/js/scramble-word.js
+++ backend/WordWhizExternalDevTunnel/src/main/resources/static/js/scramble-word.js
@@ -3,9 +3,21 @@ var scrambleWordIds = '#scramble-word1, #scramble-word2, #scramble-word3, #scram
 $(document).ready(() => {
 
 	var answerWord = null;
+	var animationForAnswer = 'animate__animated animate__flash';
+	var animationForButton = 'animate__animated animate__pulse';
 
 	$('#scramble-btn1, #scramble-btn2, #scramble-btn3, #scramble-btn4').click(function() {
 		answerWord = $(this).val();
+		$(this).css('--animate-duration', '.3s').addClass(animationForButton).one('animationend', function() {
+			$(this).removeClass(animationForButton);
+		});
+		$(".underline-word").each(function() {
+			if ($(this).hasClass('border-bottom-green')) {
+				$(this).addClass(animationForAnswer).one('animationend', function() {
+					$(this).removeClass(animationForAnswer);
+				});
+			}
+		})
 	});
 
 	$(scrambleWordIds).click(function() {
@@ -21,7 +33,6 @@ class ScrambleWordService {
 	static showQuestion(questionObj) {
 		$(scrambleWordIds).find("p").html("");
 		$(".underline-word").removeClass("border-bottom-blue").addClass("border-bottom-green");
-		$(".underline-word").addClass("animate__animated animate__headShake");
 		for (let i = 1; i <= questionObj.hint.length; i++) {
 			let word = questionObj.hint.charAt(i - 1);
 			$("#scramble-btn" + i).attr('value', word);
