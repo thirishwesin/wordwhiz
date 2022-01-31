22d2aeb409255b24a6cabe62c3874558d31471c9
Zin Bhone Htut <ZinBhoneHtut@startinpoint.com>
2022-01-31 13:33:45 +0630

    feature (animation) : add animation in scramble word

--- backend/WordWhizExternalDevTunnel/src/main/resources/static/js/scramble-word.js
+++ backend/WordWhizExternalDevTunnel/src/main/resources/static/js/scramble-word.js
@@ -2,28 +2,30 @@ var scrambleWordIds = '#scramble-word1, #scramble-word2, #scramble-word3, #scram
 
 $(document).ready(() => {
 
-  var answerWord = null;
+	var answerWord = null;
 
-  $('#scramble-btn1, #scramble-btn2, #scramble-btn3, #scramble-btn4').click(function () {
-    answerWord = $(this).val();
-  });
+	$('#scramble-btn1, #scramble-btn2, #scramble-btn3, #scramble-btn4').click(function() {
+		answerWord = $(this).val();
+	});
 
-  $(scrambleWordIds).click(function () {
-    $(this).find("p").html(answerWord);
-    $(this).find("div").removeClass("border-bottom-green").addClass("border-botton-blue");
-    var answerIndex = $(this).attr("data-wz-index");
-    submitAnswer(answerWord, answerIndex);
-  })
+	$(scrambleWordIds).click(function() {
+		$(this).find("p").html(answerWord);
+		$(this).find("div").removeClass("border-bottom-green").addClass("border-bottom-blue");
+		var answerIndex = $(this).attr("data-wz-index");
+		submitAnswer(answerWord, answerIndex);
+	})
 
 });
 
 class ScrambleWordService {
-  static showQuestion(questionObj) {
-  $(scrambleWordIds).find("p").html("");
-    for (let i = 1; i <= questionObj.hint.length; i++) {
-      let word = questionObj.hint.charAt(i - 1);
-      $("#scramble-btn" + i).attr('value', word);
-      $("#scramble-btn" + i).find("input").attr('value', word);
-    }
-  }
+	static showQuestion(questionObj) {
+		$(scrambleWordIds).find("p").html("");
+		$(".underline-word").removeClass("border-bottom-blue").addClass("border-bottom-green");
+		$(".underline-word").addClass("animate__animated animate__headShake");
+		for (let i = 1; i <= questionObj.hint.length; i++) {
+			let word = questionObj.hint.charAt(i - 1);
+			$("#scramble-btn" + i).attr('value', word);
+			$("#scramble-btn" + i).find("input").attr('value', word);
+		}
+	}
 }
