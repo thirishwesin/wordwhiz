896fc9fe91c20baeec1f8bad3efc1913d1e4a818
Zin Bhone Htut <ZinBhoneHtut@startinpoint.com>
2022-02-28 21:31:56 +0630

    enhance (scramble-word) : remove clicked button effect and selected value after paste to answer word

--- backend/WordWhizExternalDevTunnel/src/main/resources/static/js/scramble-word.js
+++ backend/WordWhizExternalDevTunnel/src/main/resources/static/js/scramble-word.js
@@ -6,14 +6,15 @@ $(document).ready(() => {
 	var answerWord = null;
 	var animationForButton = 'animate__animated animate__pulse';
 	var scrambleBtnId = "";
+	const clickedBtn = 'clicked-btn';
 
 	$(SCRAMBLE_BTN_IDS).click(function () {
 		answerWord = $(this).val();
 		scrambleBtnId = $(this).attr("id");
-		$(".btn.round2_btn").each(function(){
-			$(this).css("opacity", "");
+		$(".btn.round2_btn").each(function () {
+			$(this).removeClass(clickedBtn)
 		})
-		$(this).css({'--animate-duration': '.3s', 'opacity' : '0.5'}).addClass(animationForButton).one('animationend', function () {
+		$(this).css({ '--animate-duration': '.3s' }).addClass(`${animationForButton} ${clickedBtn}`).one('animationend', function () {
 			$(this).removeClass(animationForButton);
 		});
 		$(".underline-word").each(function () {
@@ -24,34 +25,42 @@ $(document).ready(() => {
 	});
 
 	$(SCRAMBLE_WORD_IDS).click(function () {
-		$(".answer").each(function() {
-			if($(this).siblings().first().val() === scrambleBtnId) {
-				$(this).text("")
-				$(this).parent().removeClass("border-bottom-green").addClass("border-bottom-blue");
-			}
-		})
-		$(this).find("p").html(answerWord);
-		$(this).find("div").removeClass("border-bottom-blue").addClass("border-bottom-green");
-		var answerIndex = $(this).attr("data-wz-index");
-		submitAnswer(answerWord, answerIndex, scrambleBtnId);
-		$(this).find("input").val(scrambleBtnId);
+		if (answerWord) {
+			$(".answer").each(function () {
+				if ($(this).siblings().first().val() === scrambleBtnId) {
+					$(this).text("")
+					$(this).parent().removeClass("border-bottom-green").addClass("border-bottom-blue");
+				}
+			})
+			$(SCRAMBLE_BTN_IDS).each(function () {
+				if ($(this).hasClass(clickedBtn)) {
+					$(this).removeClass(clickedBtn);
+				}
+			})
+			$(this).find("p").html(answerWord);
+			$(this).find("div").removeClass("border-bottom-blue").addClass("border-bottom-green");
+			var answerIndex = $(this).attr("data-wz-index");
+			submitAnswer(answerWord, answerIndex, scrambleBtnId);
+			$(this).find("input").val(scrambleBtnId);
+			answerWord = '';
+		}
 	})
 
 });
 
 class ScrambleWordService {
 	static showQuestion(questionObj) {
-		if(SessionUtil.getValueFromSessionStorage("questionId") != questionObj.questionId.toString()){
-			$(SCRAMBLE_WORD_IDS).each(function() {
+		if (SessionUtil.getValueFromSessionStorage("questionId") != questionObj.questionId.toString()) {
+			$(SCRAMBLE_WORD_IDS).each(function () {
 				$(SCRAMBLE_WORD_IDS).find("p").css("font-size", questionObj.fontSetting.answerFontSize).html("");
 			})
-		  }
+		}
 		$(SCRAMBLE_BTN_IDS).find("input").css("font-size", questionObj.fontSetting.questionFontSize);
 		$(".underline-word").removeClass("border-bottom-green").addClass("border-bottom-blue");
-		if(questionObj.question.length == 3) {
+		if (questionObj.question.length == 3) {
 			$("#scramble-word4").hide();
 			$("#scramble-btn4").parent().hide();
-		}else {
+		} else {
 			$("#scramble-word4").show();
 			$("#scramble-btn4").parent().show();
 		}
