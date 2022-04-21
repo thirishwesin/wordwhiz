6e09a1cbb4943dfed616f012d3f7f43184b15ced
KoKoKyaw <kokokyaw@startinpoint.com>
2022-04-21 17:55:51 +0630

    add typo word admin component

---
+++ src/app/component/typo-word/typo-word-logic/typo-word-logic.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typo-word-logic',
+  templateUrl: './typo-word-logic.component.html',
+  styleUrls: ['./typo-word-logic.component.scss']
+})
+export class TypoWordLogicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.font_size(80,80,60,60);
+  }
+  font_size(typo_q_block_f,typo_a_block_f,q_lbl_f,h_lbl_f) {
+    document.documentElement.style.setProperty("--typo-q-block-font-size", typo_q_block_f+"px");
+    document.documentElement.style.setProperty("--typo-a-block-font-size", typo_a_block_f+"px");
+    document.documentElement.style.setProperty("--q-lbl-font-size", q_lbl_f+"px");
+    document.documentElement.style.setProperty("--h-lbl-font-size", h_lbl_f+"px");
+
+    // var q_lbl_w = document.getElementById("q-block").offsetWidth;
+    // document.documentElement.style.setProperty("--q-lbl-w", q_lbl_w+"px");
+  }
+
+}
--- src/app/container/admin/admin.component.ts
+++ src/app/container/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Images } from '../../common/images';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+
+  Images = Images;
 
   constructor() { }
 
